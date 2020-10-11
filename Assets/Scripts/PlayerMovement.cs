186a7637d414c8086730b726cc2b87de10c98869
Ilya <ilya@sapronov.net>
2020-10-11 10:43:30 +0300

    Теперь не игрок движется вперед, а все объекты назад Изменение полос по свайпу, а не клику Плавное перемещение между полосами Мелкие исправления

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -4,30 +4,24 @@ using UnityEngine;
 
 public class PlayerMovement : MonoBehaviour
 {
-    private Rect rightPart = new Rect(0, 0, Screen.width / 2, Screen.height);
-    private Rect leftPart = new Rect(Screen.width / 2, 0, Screen.width / 2, Screen.height);
-    public UnityEngine.UI.Text text;
     CharacterController playerCC;
     Vector3 playerMoveVec;
     public Vector2 touchStartPos, swipeDirection=new Vector2(0,0);
-    float speed=0.1f, attackTimer=0f;
-    int lanenumber=1, enemyTimer=0, currentattack=0;
+    float  attackTimer=0f;
+    int currentLane=0, enemyTimer=0, currentAttack=0;
+    bool isForMove=false;
 
     public GameObject[] enemies;
     
     
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
+    private void Start()
     {
         playerCC = GetComponent<CharacterController>();
-        playerMoveVec = new Vector3(speed,0,0);
-        playerCC.Move(playerMoveVec);
+    }
+
+    private void Update()
+    {
+        
         if (Input.touchCount > 0)
         {
             Touch touch = Input.GetTouch(0);
@@ -37,38 +31,25 @@ public class PlayerMovement : MonoBehaviour
                     Debug.Log("Began");
                     
                     touchStartPos = touch.position;
-                    break;
-
-                case TouchPhase.Moved:
-                    Debug.Log("Move");
-                    
-                    swipeDirection = touch.position - touchStartPos;
+                    isForMove = touch.position.y <= Screen.height / 3;
                     break;
                 case TouchPhase.Ended:
-                    swipeDirection = touch.position - touchStartPos;
-
-                    if(swipeDirection.magnitude==0f){ //если клик
-                        if(leftPart.Contains(touch.position)){
-                            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Max(-1.5f, transform.position.z-1.5f));
-                        }else if(rightPart.Contains(touch.position)){
-                            transform.position = new Vector3(transform.position.x, transform.position.y, Mathf.Min(1.5f, transform.position.z+1.5f));
-                        }
-                    }
-                    
+                    swipeDirection = touch.position - touchStartPos;                    
                     break;
             }
         }
-        if(swipeDirection.magnitude>0f){
+        if(swipeDirection.magnitude>0f){            
             float dx = Mathf.Sign(swipeDirection.x); // 0 - координата не измнилась, -1 - уменьшилась (вниз или влево)
             float dy = Mathf.Sign(swipeDirection.y); //       1 увеличилась (вверх или вправо)
+            if(isForMove){
+                if(Mathf.Abs(currentLane+dx)<2) currentLane = currentLane+(int)dx;
+                isForMove=false;
+            }else{
             if(dy==1){
-                currentattack=(int)(dx)+2;
-                Debug.Log(currentattack);
-                attackTimer=30;
-                text.text=""+currentattack;
-            }else if(dy==-1) {
-                currentattack=2;
-                attackTimer=100;
+                currentAttack=(int)(dx)+2;
+            }else
+                currentAttack=2;
+            attackTimer=20;
             }
             swipeDirection=new Vector2(0,0);
         }
@@ -76,7 +57,7 @@ public class PlayerMovement : MonoBehaviour
         if(attackTimer>0f){
             attackTimer--;
         }else{
-            currentattack=0;
+            currentAttack=0;
         }
         enemyTimer++;
         if (enemyTimer%200==0)
@@ -85,11 +66,13 @@ public class PlayerMovement : MonoBehaviour
             
             Object.Instantiate(enemies[Random.Range(0,enemies.Length)],new Vector3(transform.position.x+40,transform.position.y,-1.5f+Random.Range(0,3)*1.5f), Quaternion.identity);
         }
+        playerMoveVec=new Vector3(0,0,-(transform.position.z+1.5f*currentLane)/10);
+        playerCC.Move(playerMoveVec);
 
     }
     void OnTriggerStay(Collider other)
     {
-        if (other.CompareTag("enemy"+currentattack))
+        if (other.CompareTag("enemy"+currentAttack))
             Destroy(other.gameObject);
         else
             Application.LoadLevel(Application.loadedLevel);
